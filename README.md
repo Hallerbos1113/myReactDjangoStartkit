d36a7be33cc1d13087a5779d4b4b5d21335dcb64
Hallerbos1113 <hallerbos.po@gmail.com>
2024-08-04 08:03:09 -0300

    myfirstcommit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-#myReactDjangoStartkit
+# react_django_demo_app
+A demo app for React and Django Deployment
+
+docker-compose down
+docker-compose up -d
---
+++ rdda_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\github\react_django_demo_app\rdda_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
